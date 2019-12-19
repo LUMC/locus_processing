542de8fa7fb444dd0e9a3d3851f0a6c2da141ee6
Mark Santcroos <m.a.santcroos@lumc.nl>
2019-12-19 13:16:42 +0100

    Explicitly specific loader.

--- locus_processing/__init__.py
+++ locus_processing/__init__.py
@@ -7,7 +7,7 @@ from .schemas import LocusSchema
 def load_locus_yaml(fpath: str) -> Locus:
     schema = LocusSchema()
     with open(fpath) as handle:
-        yd = yaml.load(handle)
+        yd = yaml.load(handle, Loader=yaml.SafeLoader)
     unmarshalled = schema.load(yd)
     if len(unmarshalled.errors) != 0:
         raise ValueError("Could not load Locus because {0}".format(unmarshalled.errors))
